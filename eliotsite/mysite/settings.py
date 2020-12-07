78dbad045c0d30d30c7fe2790a15c957de6e088f
Murmansker <Murmansker@users.noreply.github.com>
2020-12-07 14:56:44 +0800

    12-7

--- eliotsite/mysite/settings.py
+++ eliotsite/mysite/settings.py
@@ -116,4 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CS/u[[tatic/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
